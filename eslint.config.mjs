efc6b9697071b64412e2164faeac3c60cff9c49e
osachi <raman.asadchy@regula.us>
2025-02-16 18:47:29 +0300

    linter rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,7 +29,7 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'warn',
@@ -37,7 +37,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-member-access': 'warn',
       '@typescript-eslint/require-await': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/unbound-method': 'warn',
+      '@typescript-eslint/unbound-method': 'off',
     },
   },
 );
--- src/users/zod/filter.ts
+++ src/users/zod/filter.ts
@@ -1,16 +1,25 @@
-import { Injectable, PipeTransform, ArgumentMetadata, BadRequestException } from '@nestjs/common';
-import { z } from 'zod';
+import {
+  Injectable,
+  PipeTransform,
+  // ArgumentMetadata,
+  BadRequestException,
+} from "@nestjs/common";
+import { z } from "zod";
 
 @Injectable()
 export class ZodValidationPipe implements PipeTransform {
-    constructor(private schema: z.ZodTypeAny) {}
+  constructor(private schema: z.ZodTypeAny) {}
 
-    transform(value: any, metadata: ArgumentMetadata) {
-        try {
-            this.schema.parse(value);
-            return value;
-        } catch (error) {
-            throw new BadRequestException(error.errors);
-        }
+  transform(
+    value: any,
+    // metadata: ArgumentMetadata
+  ) {
+    try {
+      this.schema.parse(value);
+      return value;
+    } catch (error: any) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      throw new BadRequestException(error?.errors);
     }
+  }
 }
